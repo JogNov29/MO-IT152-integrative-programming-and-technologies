7ac475465df398250ecf7d208fa2981699909151
Jogine Novillas <joggggg29@gmail.com>
2025-01-17 15:49:16 +0500

    get_posts and create_posts fixed

--- connectly_project/posts/urls.py
+++ connectly_project/posts/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('users/', views.get_users, name='get_users'),
     path('users/create/', views.create_user, name='create_user'),
-    path('posts/', views.get_posts, name='get_posts'),
-    path('posts/create/', views.create_post, name='create_post'),
+    path('', views.get_posts, name='get_posts'),
+    path('create/', views.create_post, name='create_post'),
 ]
 
 
